5b9a9d48e0166f6797993157dadf279d818f1a43
Ruslan Karimov <98585383+IamKarimovich@users.noreply.github.com>
2023-04-16 13:10:33 +0400

    Initial commit

---
+++ src/main/java/com/coolab/demo/CoolabApplication.java
@@ -0,0 +1,13 @@
+package com.coolab.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coolab/demo/CoolabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolab.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
