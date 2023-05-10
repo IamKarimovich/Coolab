6e4ac7beb9725d07e80c824fa8e234975c8171e3
Ruslan Karimov <98585383+IamKarimovich@users.noreply.github.com>
2023-05-10 11:34:21 +0400

    son

--- src/main/java/com/coolab/demo/core/configuration/AppConfig.java
+++ src/main/java/com/coolab/demo/core/configuration/AppConfig.java
@@ -3,7 +3,11 @@ package com.coolab.demo.core.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
-@Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@Configuration
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AppConfig {
+
+
+
+
 }
